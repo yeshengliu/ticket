540e8dab398aae301fdc4cae8590965f2605d444
Yesheng Liu <yesheng.liu@outlook.com>
2022-12-22 03:55:22 -0800

    Configured MyBatis Generator

---
+++ src/main/java/com/yesheng/ticket/db/GeneratorSqlmap.java
@@ -0,0 +1,32 @@
+package com.yesheng.ticket.db;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+public class GeneratorSqlmap {
+
+  public void generator() throws Exception {
+    List<String> warnings = new ArrayList<>();
+    File configFile = new File("src/main/resources/generatorConfig.xml");
+    ConfigurationParser cp = new ConfigurationParser(warnings);
+    Configuration config = cp.parseConfiguration(configFile);
+    DefaultShellCallback callback = new DefaultShellCallback(true);
+    MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+        callback, warnings);
+    myBatisGenerator.generate(null);
+  }
+
+  public static void main(String[] args) throws Exception {
+    try {
+      GeneratorSqlmap generatorSqlmap = new GeneratorSqlmap();
+      generatorSqlmap.generator();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
