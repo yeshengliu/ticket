6ef6b31de70012d8be2e923010da9000044056bf
Yesheng Liu <yesheng.liu@outlook.com>
2022-12-25 05:46:36 -0800

    Generated mapper for order and user tables

---
+++ src/main/java/com/yesheng/ticket/db/mappers/OrderMapper.java
@@ -0,0 +1,17 @@
+package com.yesheng.ticket.db.mappers;
+
+import com.yesheng.ticket.db.po.Order;
+
+public interface OrderMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Order record);
+
+    int insertSelective(Order record);
+
+    Order selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(Order record);
+
+    int updateByPrimaryKey(Order record);
+}
